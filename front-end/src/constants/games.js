0ee9529f9f4cc28afa2f6f951fc771f40aec1409
danilkompaniets <kompaniets1592925@gmail.com>
2023-12-25 15:40:08 +0300

    first commotment

---
+++ front-end/src/constants/games.js
@@ -0,0 +1,8 @@
+
+const gamesList = [
+    {name: "quiz", icon: "", description: "Classical quiz game"}
+    {name: "snake", icon: "", description: "Classical snake game"}
+    {name: "tic tac toe", icon: "", description: "Classical tic tac toe game"}
+    {name: "Memory card game", icon: "", description: "Classical Memory card game"}
+
+]
